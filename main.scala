402233f65c9a40ed222420afa68f561dff89ecd0
nguyenyou <tunt081295@gmail.com>
2025-10-03 13:30:50 +0700

    🚢 ship it

--- main.scala
+++ main.scala
@@ -2,13 +2,46 @@ package main
 import scala.language.implicitConversions
 
 object Implicits {
-  transparent inline implicit def variantToVariantSelector(
+  inline implicit def variantToVariantSelector(
       v: Variant
   ): VariantSelector =
     _ => v
+
+  inline implicit def iconNameToIconNameSelector(
+      v: IconName
+  ): IconNameSelector =
+    _ => v
+
+  inline implicit def iconSignalToIconSelectorSignal(
+      s: Signal[IconName]
+  ): Signal[IconNameSelector] =
+    s.map(icon => _ => icon)
 }
 
-import Implicits.variantToVariantSelector
+object Converters {
+  given modToValue[C <: Singleton, T](using
+      v: ValueOf[C]
+  ): Conversion[C => T, T] with {
+    def apply(f: C => T): T = f(v.value)
+  }
+}
+
+trait Signal[+A] {
+  def now(): A
+  def map[B](project: A => B): Signal[B]
+}
+
+class Val[A](constantValue: A) extends Signal[A] {
+  def now(): A = constantValue
+  def map[B](project: A => B): Signal[B] = new Val(project(constantValue))
+}
+
+import Implicits.{
+  variantToVariantSelector,
+  iconNameToIconNameSelector,
+  iconSignalToIconSelectorSignal
+}
+// import Converters.modToValue
 
 enum Variant {
   case Primary, Secondary
@@ -18,21 +51,42 @@ enum Size {
   case Small, Medium, Large
 }
 
-type VariantSelector = Variant.type => Variant
-type SizeSelector = Size.type => Size
+enum IconName {
+  case Home, Search, Settings, Empty
+}
 
-case class Button(variant: Variant, size: Size)
+type VariantSelector = Variant.type => Variant
+
+extension (value: Variant) {
+  inline def selector: VariantSelector = _ => value
+}
+
+extension (selector: VariantSelector) {
+  inline def value: Variant = selector(Variant)
+}
+
+type SizeSelector = Size.type => Size
+type IconNameSelector = IconName.type => IconName
+
+case class Button(variant: Variant, size: Size, icon: Signal[IconName])
 
 object Button {
-  transparent inline def apply(
+  inline def apply(
       inline variant: VariantSelector = _.Primary,
-      inline size: SizeSelector = _.Medium
+      inline size: SizeSelector = _.Medium,
+      inline icon: Signal[IconNameSelector] = Val(_.Empty)
   ): Button = {
-    Button(variant = variant(Variant), size = size(Size))
+    Button(
+      variant = variant(Variant),
+      size = size(Size),
+      icon = icon.map(_(IconName))
+    )
   }
 }
 
 @main def run() = {
-  val button3 = Button(variant = Variant.Secondary)
-  println(s"Regular usage (has default lambda): $button3")
+  val iconSignal: Signal[IconName] = Val(IconName.Home)
+
+  val button4 = Button(variant = Variant.Secondary, icon = iconSignal)
+  println(button4)
 }
